e437ff86747436832e3bae4028cabb8bca24984a
BenBagby <benbagby5@gmail.com>
2020-06-09 13:54:09 -0600

    removed need for temp folder

--- Parser/ParserOCR.py
+++ Parser/ParserOCR.py
@@ -8,26 +8,27 @@ import tempfile
 PDF_file = 'Parser/Input/2500_20040002.pdf'
 
 with tempfile.TemporaryDirectory() as path:
+    print(path)
 
     pages = convert_from_path(PDF_file, output_folder=path)
     image_counter = 1
     for page in pages:
-        filename = "Parser/Temp/page_" + str(image_counter) + ".jpg"
+        filename = path+"/page_" + str(image_counter) + ".jpg"
         print(filename)
         page.save(filename, 'JPEG')
         image_counter += 1
 
-filelimit = image_counter - 1
-outfile = "Parser/Output/out_text.txt"
+    filelimit = image_counter - 1
+    outfile = "Parser/Output/out_text.txt"
 
-f = open(outfile, "a")
+    f = open(outfile, "a")
 
-for i in range(1, filelimit + 1):
-    filename = "Parser/Temp/page_"+str(i)+".jpg"
-    text = str(pytesseract.image_to_string(Image.open(filename)))
-    text = text.replace('-\n', '')
-    f.write(text)
+    for i in range(1, filelimit + 1):
+        filename = path+"/page_"+str(i)+".jpg"
+        text = str(pytesseract.image_to_string(Image.open(filename)))
+        text = text.replace('-\n', '')
+        f.write(text)
 
-f.close()
+    f.close()
 
 
